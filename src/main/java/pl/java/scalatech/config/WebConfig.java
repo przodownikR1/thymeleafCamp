dc68b5f002f48da1b50d1ebf66e47c4ac0b48f9a
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-17 11:20:45 +0100

    switch language + repair messageResource

--- src/main/java/pl/java/scalatech/config/WebConfig.java
+++ src/main/java/pl/java/scalatech/config/WebConfig.java
@@ -1,20 +1,53 @@
 package pl.java.scalatech.config;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter{
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
     @Bean(name = AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("i18n/messages");
-        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setUseCodeAsDefaultMessage(false);
         return messageSource;
     }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.forLanguageTag(env.getProperty("language")));
+        return clr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
 }
--- src/main/java/pl/java/scalatech/web/controller/HelloWorldController.java
+++ src/main/java/pl/java/scalatech/web/controller/HelloWorldController.java
@@ -1,13 +1,24 @@
 package pl.java.scalatech.web.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
-    String hello(){
+    @Autowired
+    MessageSource ms;
+
+    @RequestMapping(value ="/hello")
+    String hello( Locale locale){
+         log.info("+++  {}",ms.getMessage("hello.world", null, locale));
          return "hello";
     }
 
