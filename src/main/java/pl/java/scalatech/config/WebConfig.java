f88087a1b215ea25106b4466ae69299e1fcfd23c
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-17 13:21:58 +0100

    clean with registry.addResourceHandler

--- src/main/java/pl/java/scalatech/config/WebConfig.java
+++ src/main/java/pl/java/scalatech/config/WebConfig.java
@@ -13,12 +13,16 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
@@ -51,14 +55,36 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return lci;
     }
 
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(31556926);
-        registry.addResourceHandler("/favicon.ico").addResourceLocations("WEB-INF/static/favicon.ico");
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(0);
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/").setCachePeriod(0);
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/resources/images/").setCachePeriod(0);
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/").setCachePeriod(0);
 
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/welcome").setViewName("welcome");
+        registry.addViewController("/").setViewName("welcome");
+     //   registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("logout");
+        registry.addViewController("/accessdenied").setViewName("accessdenied");
+    }
+
+    /* @Controller
+    static class FaviconController {
+        @RequestMapping("favicon.ico")
+        String favicon() {
+            log.info("+++++       favicon");
+            return "forward:/static/favicon.ico";
+        }
+    }*/
 }
