2547f9a0c79142412a68a3c542bef4731bcf4026
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-17 11:06:25 +0100

    MessageSource

---
+++ src/main/java/pl/java/scalatech/config/WebConfig.java
@@ -0,0 +1,20 @@
+package pl.java.scalatech.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Bean(name = AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+}
