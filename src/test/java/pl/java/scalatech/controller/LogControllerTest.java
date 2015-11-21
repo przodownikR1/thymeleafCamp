d09497056586a9c4f9f46c619dc45253bfe6bf41
Slawomir Borowiec <przodownik@tlen.pl>
2015-11-21 23:17:52 +0100

    gzip start

--- src/test/java/pl/java/scalatech/controller/LogControllerTest.java
+++ src/test/java/pl/java/scalatech/controller/LogControllerTest.java
@@ -2,16 +2,13 @@ package pl.java.scalatech.controller;
 
 import java.util.Locale;
 
-import org.junit.Rule;
 import org.junit.Test;
 
-import pl.java.scalatech.tools.LogbackVerifier;
+//import pl.java.scalatech.tools.LogbackVerifier;
 import pl.java.scalatech.web.controller.HelloWorldController;
 
 public class LogControllerTest {
 
-    @Rule
-    public LogbackVerifier logbackVerifier = new LogbackVerifier();
 
     @Test
     public void shouldLogCatch() {
